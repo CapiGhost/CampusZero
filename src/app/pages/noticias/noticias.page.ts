4c8f77a5f637f70196c2f5029d97297fd2aa0120
Bikembe <bikembecs@gmail.com>
2025-10-10 11:42:07 -0600

    Initial commit

---
+++ src/app/pages/noticias/noticias.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-noticias',
+  templateUrl: './noticias.page.html',
+  styleUrls: ['./noticias.page.scss'],
+  standalone: false,
+})
+export class NoticiasPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
